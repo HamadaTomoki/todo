750f0414f727ed91e901bea28cf529cb67e5d715
hamadatomoki <hamada.tomoki01@gmail.com>
2023-03-13 23:26:59 +0900

    :sparkles: feat: add database configure

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,59 @@
-use anyhow::{anyhow, Context, Result};
-use notion::{ids::DatabaseId, NotionApi};
+use anyhow::{anyhow, Context, Ok, Result};
+use clap::Parser;
+use config::Config;
+use notion::{ids::DatabaseId, models::Database, NotionApi};
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize)]
+#[derive(Parser, Debug)]
+#[clap(version = "1.0", author = "Jake Swenson")]
+struct Opts {
+    #[clap(subcommand)]
+    command: SubCommand,
+}
+#[derive(Parser, Debug)]
+enum SubCommand {
+    List,
+    Add,
+}
+#[derive(Deserialize, Serialize, Clone)]
 struct TodoConfig {
     api_token: Option<String>,
-    task_database_id: Option<DatabaseId>,
+    database_id: Option<DatabaseId>,
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let config = config::Config::default()
-        .with_merged(config::File::with_name("Secrets"))
-        .unwrap_or_default()
-        .with_merged(config::Environment::with_prefix("NOTION"))?;
-
-    let config: TodoConfig = config.try_into().context("Failed to read config.")?;
-    let notin_api = NotionApi::new(
-        std::env::var("NOTION_API_TOKEN")
-            .or(config.api_token.ok_or(anyhow!("No api token from config")))
-            .context(
-                "No Notion API token found in either the environment variable \
-                        `NOTION_API_TOKEN` or the config file!",
-            )?,
+    // Setup Notion API
+    let opts = Opts::parse();
+    let config = config::Config::builder()
+        .add_source(config::File::with_name("Secrets"))
+        .add_source(config::Environment::with_prefix("NOTION"))
+        .build()?;
+    let config: TodoConfig = config.try_deserialize().context("")?;
+    let notion_api = NotionApi::new(
+        config
+            .clone()
+            .api_token
+            .expect("Failed to read NOTION_API_TOKEN"),
     )?;
+
+    // Get database instance from database_id
+    let database_id = config
+        .clone()
+        .database_id
+        .expect("Failed to read NOTION_DATABASE_ID");
+    let database: Database = notion_api.get_database(database_id).await?;
+
+    // Use command
+    match opts.command {
+        SubCommand::List => list_tasks(database),
+        SubCommand::Add => add_task(database),
+    }
+}
+
+fn list_tasks(db: Database) -> Result<()> {
+    Ok(())
+}
+fn add_task(db: Database) -> Result<()> {
     Ok(())
 }
