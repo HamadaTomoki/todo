719556f9de7803116bfa57138ed25a32e2ba384b
hamadatomoki <hamada.tomoki01@gmail.com>
2023-03-16 14:49:48 +0900

    :sparkles: feat: add list command prototype

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use anyhow::{anyhow, Context, Ok, Result};
+use anyhow::{Context, Ok, Result};
 use clap::Parser;
 use config::Config;
-use notion::{ids::DatabaseId, models::Database, NotionApi};
+use notion::{ids::DatabaseId, models::search::DatabaseQuery, NotionApi};
 use serde::{Deserialize, Serialize};
 
 #[derive(Parser, Debug)]
-#[clap(version = "1.0", author = "Jake Swenson")]
+#[clap(version = "1.0", author = "novum")]
 struct Opts {
     #[clap(subcommand)]
     command: SubCommand,
@@ -21,15 +21,25 @@ struct TodoConfig {
     database_id: Option<DatabaseId>,
 }
 
+// #[derive(Debug)]
+// struct Task {
+//     name: PropertyValue,
+//     status: PropertyValue,
+//     category: PropertyValue,
+//     project: PropertyValue,
+//     date: PropertyValue,
+//     mind: PropertyValue,
+// }
+
 #[tokio::main]
 async fn main() -> Result<()> {
     // Setup Notion API
     let opts = Opts::parse();
-    let config = config::Config::builder()
+    let config = Config::builder()
         .add_source(config::File::with_name("Secrets"))
         .add_source(config::Environment::with_prefix("NOTION"))
         .build()?;
-    let config: TodoConfig = config.try_deserialize().context("")?;
+    let config: TodoConfig = config.try_deserialize().context("Failed to read config")?;
     let notion_api = NotionApi::new(
         config
             .clone()
@@ -42,18 +52,28 @@ async fn main() -> Result<()> {
         .clone()
         .database_id
         .expect("Failed to read NOTION_DATABASE_ID");
-    let database: Database = notion_api.get_database(database_id).await?;
 
     // Use command
     match opts.command {
-        SubCommand::List => list_tasks(database),
-        SubCommand::Add => add_task(database),
+        SubCommand::List => list_tasks(notion_api, database_id).await,
+        SubCommand::Add => add_task(notion_api),
     }
 }
 
-fn list_tasks(db: Database) -> Result<()> {
+async fn list_tasks(notion_api: NotionApi, database_id: DatabaseId) -> Result<()> {
+    let query = DatabaseQuery::default();
+    let response = notion_api.query_database(database_id, query).await?;
+    response.results.iter().for_each(|x| {
+        x.properties
+            .properties
+            .iter()
+            .for_each(|y| println!(" {}\n{:?}\n\n", y.0, y.1));
+        println!("---------------------------------------------------------------------------------------------------------------------");
+    });
     Ok(())
 }
-fn add_task(db: Database) -> Result<()> {
+
+fn add_task(_notion_api: NotionApi) -> Result<()> {
+    // let req =  PageCreateRequest { parent: notion::models::Parent::Page { page_id: () }}
     Ok(())
 }
